a0ad772098d46d485e45cd6d94d304c00bb28944
DELL <DELL@DESKTOP-B7O01NL>
2025-06-27 15:49:44 +0530

    day1

---
+++ day1/src/day1/firstday.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class firstday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println(".....hello friends......");
+	}
+
+}
