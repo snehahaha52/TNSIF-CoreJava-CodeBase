0a8c005eae00a5e1daded9bc762628453e2c99d5
snehahaha52 <navgire168@gmail.com>
2025-07-07 21:54:05 +0530

    Add files via upload
    day7

---
+++ Day7/src/Overriding/covarientOverridingDemo.java
@@ -0,0 +1,46 @@
+package Overriding;
+
+//Parent class
+class Colour{
+
+	protected Colour getColour()
+	{
+		Colour s=new Colour();
+		return s;
+	}
+}
+//child class
+class Red extends Colour{
+	
+	@Override
+	protected Red getColour()
+	{
+		Red s=new Red();
+		return s;
+	}
+	
+	
+}
+//child class
+class Blue extends Colour{
+	
+	@Override
+	protected Blue getColour()
+	{
+		Blue s=new Blue();
+		return s;
+	}
+	
+	
+}
+
+
+
+public class covarientOverridingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
